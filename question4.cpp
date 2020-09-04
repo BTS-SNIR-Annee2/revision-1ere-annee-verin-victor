dd825bfdfdf2999629573a567adeda0f68bd639b
verin-victor <55384802+verin-victor@users.noreply.github.com>
2020-09-04 15:49:10 +0200

    question4 ok

---
+++ question4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string prenom;
+
+	cout << "Quel est votre nom ?" << endl;
+	cin >> prenom;
+	cout << "Bonjours, " << prenom << endl;
+
+}
