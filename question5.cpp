02f75c39e089769f521665d1b33e16976cda601e
verin-victor <55384802+verin-victor@users.noreply.github.com>
2020-09-04 16:46:22 +0200

    question5 avec le nombre d'article

--- question5.cpp
+++ question5.cpp
@@ -6,14 +6,17 @@ int main()
 	float HT;
 	float tva;
 	float TTC;
+	int nbarticle;
 
+	cout << "Saisir le nombre d'article" << endl;
+	cin >> nbarticle;
 	cout << "Saisir prix HT" << endl;
 	cin >> HT;
 	cout << "Saisir tva (en %)" << endl;
 	cin >> tva;
 
 	TTC = HT + (HT / 100 * tva);
+	TTC = TTC * nbarticle;
 
-	cout << "Le prix TTC est de " << TTC << " euros avec une tva a " << tva << "% et un prix HT de " << HT << " euros" << endl;
+	cout << "Le prix TTC est de " << TTC << " euros avec une tva a " << tva << "% et un prix HT de " << HT << " euros, pour " << nbarticle << " article" << endl;
 
-}
