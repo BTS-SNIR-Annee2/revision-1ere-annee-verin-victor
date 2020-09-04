828903f1a7fcc0043a87de2d3d459d43677e2066
verin-victor <55384802+verin-victor@users.noreply.github.com>
2020-09-04 15:42:15 +0200

    question3 ok

---
+++ question3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float nbuser;
+	float nbfinal;
+
+	cout << "Saisir le nombre a mettre au carre:" << endl;
+	cin >> nbuser;
+
+	nbfinal = nbuser * nbuser;
+
+	cout << "Le carre est: " << nbfinal <<endl;
+
+}
